0663b02c447492da79b98cb48204033257850be0
Ross Jacobs <rj@swit.sh>
2020-05-22 14:48:28 -0700

    Bug fixes

--- bosspiles.py
+++ bosspiles.py
@@ -31,7 +31,7 @@ async def on_message(message):
         if args[0] in ["add", "remove"] and len(args) != 2:
             await message.channel.send(f"`!bp {args[1]}` requires 3 arguments. See `!bp`.")
             return
-        if args[0] in ["victory", "active"] and len(args) != 3:
+        if args[0] in ["win", "active"] and len(args) != 3:
             await message.channel.send(f"`!bp {args[1]}` requires 4 arguments. See `!bp`.")
             return
         pins = await message.channel.pins()
@@ -41,30 +41,33 @@ async def on_message(message):
         bp_message = None
         edit_existing_bp = False
         for pin in pins:
-            print("Found pin", pin.content)
             if ":crown:" in pin.content and ":arrow_double_up:" in pin.content:
                 bp_message = pin
                 # We can only edit our own messages
                 edit_existing_bp = pin.author == client.user
                 break
         if not bp_message:
-            await message.channel.send("This channel has no bosspile! Pin a message with your bosspile and try again.")
+            await message.channel.send("This channel has no bosspile! Pin a your bosspile message and try again.")
             return
         bosspile = BossPile("Can't stop", bp_message.content)
-        if args[0] == "victory":
+        if args[0] == "win":
             victor = args[1]
             loser = args[2]
-            bosspile.victory(victor, loser)
+            win_messages = bosspile.win(victor, loser)
+            [await message.channel.send(m) for m in win_messages]
         elif args[0] == "add":
             player_name = args[1]
             bosspile.add(player_name)
+            await message.channel.send(f"{player_name} has been added.")
         elif args[0] == "remove":
             player_name = args[1]
             bosspile.remove(player_name)
+            await message.channel.send(f"{player_name} has been removed.")
         elif args[0] == "active":
             player_name = args[1]
             state = args[2].lower() == "true"
             bosspile.change_active_status(player_name, state)
+            await message.channel.send(f"{player_name} is now {'in'*(not state)}active.")
         else:
             await message.channel.send(f"Unrecognized command {args[1]}. Run `!bp`.")
         new_bosspile = bosspile.generate_bosspile()
@@ -110,36 +113,65 @@ class BossPile:
         self.game = game
         self.players = self.parse_bosspile(bosspile_text)
 
-    def victory(self, p1, p2):
+    def win(self, p1, p2):
         """p1 has won the game. p1 is climbing. p2 stops climbing."""
-        p1_pos = 0
-        p2_pos = 0
+        p1_pos = -1
+        p2_pos = -1
         for i in range(len(self.players)):
             player = self.players[i]
             if player.username == p1:
                 p1_pos = i
-                self.players[i].climbing = True
             if player.username == p2:
                 p2_pos = i
-                self.players[i].climbing = False
+        if p1_pos == -1:
+            return [f"`{p1}` is not a valid player name. You may need to quote."]
+        if p2_pos == -1:
+            return [f"`{p2}` is not a valid player name. You may need to quote."]
+        if not (self.players[p1_pos].climbing ^ self.players[p2_pos].climbing):
+            return ["An invalid match was detected: 0 or 2 climbers found. No changes made."]
+        if abs(p1_pos - p2_pos) != 1:
+            return ["An invalid match was detected: Players are not adjacent. No changes made."]
+        messages = []
+        self.players[p1_pos].climbing = True
+        self.players[p2_pos].climbing = False
+        # If user is boss and loses, move to bottom and convert 5 orange => blue
+        if p2_pos == 0:
+            messages += [self.players[p2_pos].username + " has lost the :crown: to " + self.players[p1_pos].username]
+            blue_diamonds = self.players[p2_pos].orange_diamonds // 5
+            if blue_diamonds > 0:
+                self.players[p2_pos].blue_diamonds += blue_diamonds
+                messages += [self.players[p2_pos].username + " has gained a :large_blue_diamond: and descends"]
+            self.players[p2_pos].orange_diamonds %= 5
+            self.players[p2_pos].climbing = True
+            self.players = self.players[1:] + [self.players[0]]  # move player to end
         # If winner is higher in array (lower in ladder), players switch places
-        if p1_pos > p2_pos:
+        elif p1_pos > p2_pos:
             temp = self.players[p1_pos]
             self.players[p1_pos] = self.players[p2_pos]
             self.players[p2_pos] = temp
+            messages += [self.players[p2_pos].username + " has lost to " + self.players[p1_pos].username]
         # If user is boss and wins, add an orange diamond
         if p1_pos == 0:
+            messages += [self.players[p1_pos].username + " has defended the :crown: and gains :small_orange_diamond:"]
             self.players[p1_pos].orange_diamonds += 1
-        # If user is boss and loses, move to bottom and convert 5 orange => blue
-        if p2_pos == 0:
-            self.players[p2_pos].blue_diamonds += self.players[p2_pos].orange_diamonds // 5
-            self.players[p2_pos].orange_diamonds %= 5
-            self.players = self.players[1:] + [self.players[0]]  # move player to end
+        new_matches = self.generate_matches()
+        for match in new_matches:
+            messages += [f"{self.players[match[0]].username} :crossed_swords: {self.players[match[1]].username}"]
+        return messages
+
+    def generate_matches(self):
+        """Create the matches based on who is climbing."""
+        matches = []
+        for i in range(len(self.players)):
+            if i > 0 and self.players[i].climbing and not self.players[i-1].climbing:
+                matches.append((i, i-1))
+        return matches
 
     def add(self, player_name):
         """Add a player to the very end."""
         new_player = PlayerData(player_name)
         self.players.append(new_player)
+        self.players[-1].climbing = True
 
     def remove(self, player_name):
         """Delete a player from the leaderboard."""
@@ -167,7 +199,8 @@ class BossPile:
         # regex: https://regex101.com/r/iF4cVx/1
         regex = re.compile(r"(?:^|\n)\s*(?::[a-z_]*: ?)* *(\w[a-zA-Z0-9 ]+\w) *(?::[a-z_]*:)*")
         all_player_data = []
-        for player_text in player_lines:
+        for i in range(len(player_lines)):
+            player_text = player_lines[i]
             orange_diamonds = player_text.count(":small_orange_diamond:")
             orange_diamonds += 5 * player_text.count(":large_orange_diamond:")
             blue_diamonds = player_text.count(":large_blue_diamond:")
@@ -202,6 +235,8 @@ class BossPile:
                 prev_player_climbing = player.climbing
             bosspile_text += '\n'
         return bosspile_text
+
+
 
 
 client.run(TOKEN)
--- test.py
+++ test.py
@@ -6,8 +6,8 @@ from examples import example_cantstop_bosspile
 def test():
     """main test"""
     bosspile = BossPile("Can't Stop", example_cantstop_bosspile)
-    bosspile.victory("Lotus blossum", "joepunman")
-    bosspile.victory("joepunman", "salaozy")
+    bosspile.win("Lotus blossum", "joepunman")
+    bosspile.win("joepunman", "salaozy")
     print(bosspile.generate_bosspile())
 
 
